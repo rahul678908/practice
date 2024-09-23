25b76201fb4cd23fddeec34e8e06fe6515cf6ee5
rahul678908 <rahulkannan33886@gmail.com>
2024-09-23 13:00:48 +0530

    Add files via upload

---
+++ excercises1.py
@@ -0,0 +1,16 @@
+a = ('twinkle twinkle little star')
+b = ('how i wonder what you are')
+c = ('up above the world so high')
+d = ('like a diamond in the sky')
+print(a)
+print(b)
+print(c)
+print(d)
+
+#to get the python bersion
+
+import sys
+print("python version")
+print(sys.version)
+print('version')
+print(sys.version_info)
